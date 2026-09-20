c3095966ef428a98f6a075f49e0d9ec66a6f8d1b
Augusto Pereira <nortevan@outlook.com>
2026-09-19 17:11:21 -0700

    sql proj added

--- SQL/ExchangeRates.SQL/GetProvider.sql
+++ SQL/ExchangeRates.SQL/GetProvider.sql
@@ -7,3 +7,13 @@ LEFT JOIN Currency sc ON sc.Id = cbsc.CurrencyId
 WHERE nc.CurrencyCode IN ('AOA', 'MGA', 'MDL', 'MAD')
    OR sc.CurrencyCode IN ('AOA', 'MGA', 'MDL', 'MAD')
 ORDER BY cb.BankCode, sc.CurrencyCode;
+
+SELECT *
+FROM CentralBank cb
+JOIN Currency nc ON nc.Id = cb.CurrencyId
+
+WHERE nc.CurrencyCode = 'AOA';
+
+update CentralBank
+set ApiUrl = 'https://www.bna.ao/service/rest/taxas/get/evolucao/taxa/intervalo'
+where BankCode = 'BNA';
